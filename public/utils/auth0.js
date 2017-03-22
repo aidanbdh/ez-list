1366500bd2fa2c7cf2929cf28de244f28ea99996
Sarah Gaeta <Sarah Gaeta>
2017-03-22 03:28:24 -0700

    Merging #1

--- public/utils/auth0.js
+++ public/utils/auth0.js
@@ -1,3 +1,49 @@
 const Auth0Lock = require('auth0-lock')
 
+class AuthService {
+  constructor(clientId, domain) {
+    // Configure Auth0
+    this.lock = new Auth0Lock(clientId, domain, {
+      auth: {
+        redirectUrl: 'http://localhost:3000/login',
+        responseType: 'token'
+      }
+    })
+    // Add callback for lock `authenticated` event
+    this.lock.on('authenticated', this._doAuthentication.bind(this))
+    // binds login functions to keep this context
+    this.login = this.login.bind(this)
+  }
+
+  _doAuthentication(authResult) {
+    // Saves the user token
+    this.setToken(authResult.idToken)
+  }
+
+  login() {
+    // Call the show method to display the widget.
+    this.lock.show()
+  }
+
+  loggedIn() {
+    // Checks if there is a saved token and it's still valid
+    return !!this.getToken()
+  }
+
+  setToken(idToken) {
+    // Saves user token to local storage
+    localStorage.setItem('id_token', idToken)
+  }
+
+  getToken() {
+    // Retrieves the user token from local storage
+    return localStorage.getItem('id_token')
+  }
+
+  logout() {
+    // Clear user token and profile data from local storage
+    localStorage.removeItem('id_token');
+  }
+}
+
 module.exports = { lock: new Auth0Lock(process.env.AUTH0_CLIENTID || 'GL4oJ1GSV0r6lXAR9W56R3dEmQ3lRu55', 'aidanbdh.auth0.com') }
